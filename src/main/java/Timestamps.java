f316b26ff6a5510fa7e379eeeaed5bc34f27d68f
IdanMalkaQ <70653770+IdanMalkaQ@users.noreply.github.com>
2020-09-04 11:54:54 -0700

    Update Timestamps.java

--- src/main/java/Timestamps.java
+++ src/main/java/Timestamps.java
@@ -1,26 +1,47 @@
 import java.util.Scanner;
 
 class Timestamps {
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
+    // Creates a new scanner for user input
+    Scanner in = new Scanner(System.in);
 
-        //Reads ints from user
-        int hours1 = scanner.nextInt();
-        int minutes1 = scanner.nextInt();
-        int seconds1 = scanner.nextInt();
-        int hours2 = scanner.nextInt();
-        int minutes2 = scanner.nextInt();
-        int seconds2 = scanner.nextInt();
+    int hourSum;
+    int minuteSum;
+    int secondSum;
 
+    // Reads ints from user
+    System.out.println("Enter your 1st hour: ");
+    int hours1 = in.nextInt();
+    System.out.println("Enter your 1st minutes: ");
+    int minutes1 = in.nextInt();
+    System.out.println("Enter your 1st seconds: ");
+    int seconds1 = in.nextInt();
+    System.out.println("Enter your 2nd hour: ");
+    int hours2 = in.nextInt();
+    System.out.println("Enter your 2nd minutes: ");
+    int minutes2 = in.nextInt();
+    System.out.println("Enter your 2nd seconds: ");
+    int seconds2 = in.nextInt();
 
-        /*
-         *  your code goes here
-         */
+    /*
+     * your code goes here
+     */
+    hours1 = hours1 * 3600;
+    hours2 = hours2 * 3600;
+    hourSum = hours1 - hours2;
 
+    minutes1 = minutes1 * 60;
+    minutes2 = minutes2 * 60;
+    minuteSum = minutes1 - minutes2;
 
-        // closing the scanner object
-        scanner.close();
-    }
+    secondSum = seconds1 - seconds2;
+
+    System.out.println(hourSum);
+    System.out.println(minuteSum);
+    System.out.println(secondSum);
+    System.out.println((hourSum + minuteSum + secondSum) * -1);
+    // closing the scanner object
+    in.close();
+  }
 }
