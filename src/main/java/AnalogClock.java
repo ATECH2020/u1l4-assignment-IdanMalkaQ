9acf2c9912301648ab9e84b69b1b4ffce3e771c8
IdanMalkaQ <70653770+IdanMalkaQ@users.noreply.github.com>
2020-09-04 11:53:07 -0700

    Update AnalogClock.java

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -4,16 +4,61 @@ class AnalogClock {
     public static void main(String[] args) {
 
         //Creates a new scanner for user input
-        Scanner scanner = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         //Reads ints from user
-
-
-        /*
-         *  your code goes here
-         */
+        System.out.println("Enter what angle is the hour hand at: ");
+        double hourAngle = in.nextDouble(); 
+        
+        double minuteAngle;
+        double minute = 0;
+        if(hourAngle > 0 && hourAngle <= 90)
+        {// time frame 1 - 3
+            hourAngle = hourAngle % 30;
+            minute = hourAngle / .5;
+            System.out.println(minute);
+        }else if(hourAngle > 90 && hourAngle <= 180)
+        {// time frame 3 - 6
+            hourAngle = hourAngle % 30;
+            minute = hourAngle / .5;
+            System.out.println(minute);
+        }else if(hourAngle > 180 && hourAngle <= 270)
+        {// time frame 6 - 9
+            hourAngle = hourAngle % 30;
+            minute = hourAngle / .5;
+            System.out.println(minute);        
+        }else if(hourAngle > 270 && hourAngle <= 360)
+        {// time frame 9 - 12
+            hourAngle = hourAngle % 30;
+            minute = hourAngle / .5;
+            System.out.println(minute);            
+        }
+        
+        
+        
+        if(minute > 0 && hourAngle <= 15)
+        {// time frame 1 - 3
+            // angle 1 - 90
+            minuteAngle = minute * 6;
+            System.out.println(minuteAngle);
+        }else if(hourAngle > 15 && hourAngle <= 30)
+        {// time frame 3 - 6
+            // angle 91 - 180
+            minuteAngle = minute * 6;
+            System.out.println(minuteAngle);
+        }else if(hourAngle > 30 && hourAngle <= 45)
+        {// time frame 6 - 9
+            // angle 181 - 270
+            minuteAngle = minute * 6;
+            System.out.println(minuteAngle);             
+        }else if(hourAngle > 45 && hourAngle <= 60)
+        {// time frame 9 - 12
+            // angle 271 - 360
+            minuteAngle = minute * 6;
+            System.out.println(minuteAngle);            
+        }
 
         // closing the scanner object
-        scanner.close();
+        in.close();
     }
 }
